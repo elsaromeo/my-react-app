46d13981b4c3517e0941d8065c2318a12742e341
Elsa <elsaromeo10@gmail.com>
2021-05-01 08:58:00 +0200

    app.js change

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import "./Styles.css";
 import "./App.css";
 import Weather from "./Weather";
-
-
-
 export default function App() {
   return (
     <div className="App">
@@ -11,17 +8,24 @@ export default function App() {
         <Weather defaultCity="Milan" />
         <br />
       </div>
-      
-       <footer id="footer">
-          This project was coded by <a href="https://nostalgic-bhabha-b4cffb.netlify.app/"
-          target="_blank" rel="noreferrer"/>Elsa Romeo and is {" "}
-          <a
-            href="https://github.com/elsaromeo/my-react-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >open-sourced on GitHub</a>
-        </footer>
-        
+      <footer id="footer">
+        This project was coded by Elsa Romeo and is{" "}
+        <a
+          href="https://github.com/elsaromeo/my-react-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open-sourced on GitHub
+        </a>{" "}
+        and hosted on{" "}
+        <a
+          href="https://nostalgic-bhabha-b4cffb.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify
+        </a>
+      </footer>
     </div>
   );
 }
